00eb4288892cd0eceb425e9290517b6aa3d668e3
orejuela <jefferson.orejuela.sannicolas@utelvt.edu.ec>
2022-09-16 10:18:24 -0500

    programa creado para calcular velocidad

---
+++ velocimetro.cpp
@@ -0,0 +1,14 @@
+//programa para calcular velocidad
+//nombre:jefferson orejuela
+//fecha:16-09-2022
+#include<iostream>
+using namespace std;
+int main()
+{
+	float v,d,t;
+	cout<<"ingrese la distancia(km)= "; cin>>d;
+	cout<<"ingrese el tiempo (hora) t= "; cin>>t;
+	v=d/t;
+	cout<<"la velocidad calculada es de v= "<<t<<endl<<endl;
+	return 0;
+}
